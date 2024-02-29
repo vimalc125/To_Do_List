4d50bad4121c934df181d6b683fe5736bef2abfc
Vimal Chinnappa <157469649+vimalc125@users.noreply.github.com>
2024-02-29 19:46:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,127 @@
+import Content from './Content';
+import Header from './Header';
+import Footer from './Footer';
+import { useState, useEffect } from 'react';
+import Additem from './Additem';
+import SearchItem from './SearchItem';
+import apiRequest from './apiRequest';
+
+function App() {
+
+  const API_URL = 'http://localhost:3500/items';
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('')
+  const [search, setSearch] = useState('')
+  const [fetchError, setFetchError] = useState(null)
+  const [isLoading, setIsLoading]= useState(true)
+
+useEffect(() => {
+  const fetchItems = async () => {
+    try {
+      const response = await fetch(API_URL);
+      if(!response.ok) throw Error("Data not received");
+      const listItems = await response.json();
+      setItems(listItems)
+      setFetchError(null) 
+    } 
+    catch (err){
+      setFetchError(err.message)
+    }
+    finally{
+      setIsLoading(false)
+    }
+  }
+  setTimeout(() => {
+  (async () => await fetchItems())()
+}, 2000)
+}, [])
+
+const addItem = async(item) => {
+  const id = items.length ? items[items.length - 1].id+1 : 1;
+  const addNewItem = {id, checked: false, item}
+  const listItems= [...items, addNewItem]
+  setItems(listItems)
+
+  const postOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(addNewItem)
+  }
+  
+  const result = apiRequest(API_URL,postOptions)
+  if(result) setFetchError(result)   
+}
+
+const handleCheck = async (id) => {
+    const listItems = items.map((item) => 
+    item.id===id ? {...item, checked:!item.checked} : item)
+    setItems(listItems)
+    
+    const myItem = listItems.filter((item) => item.id===id)
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({checked:myItem[0].checked})
+    }
+
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl,updateOptions)
+    if(result) setFetchError(result)
+
+}
+
+const handleDelete = async (id) => {
+    const listItems = items.filter((item) =>
+    item.id!==id)
+    setItems(listItems)
+
+    const deleteOptions = {method: `DELETE`}
+    
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl,deleteOptions)
+    if(result) setFetchError(result)
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  if(!newItem) return;
+  console.log(newItem)
+  addItem(newItem)
+  setNewItem('')
+}
+
+  return (
+    <div className="App">
+      <Header title = "To Do List"/>
+      <Additem 
+      newItem= {newItem}
+      setNewItem= {setNewItem}
+      handleSubmit= {handleSubmit}
+      />
+
+      <SearchItem
+      search={search}
+      setSearch={setSearch}
+      />
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p> {`Error: ${fetchError}`}</p>}
+      {!isLoading && !fetchError&& <Content 
+        items = {items.filter(item => 
+           ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />}
+      </main>
+      <Footer 
+        length = {items.length}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ Content.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import ItemsList from './ItemsList';
+
+const Content = ({items, handleCheck, handleDelete}) => 
+{
+    
+  return (
+    <>
+        {(items.length) ? (
+            <ItemsList
+            items = {items}
+            handleCheck= {handleCheck}
+            handleDelete= {handleDelete}
+            />
+        
+        ) : (
+            <p style = {{marginTop: '2rem'}}> Your List is Empty </p>
+        )
+  }
+
+    </>
+  )
+}
+
+export default Content
